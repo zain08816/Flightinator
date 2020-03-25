074e32e1894bd7e6ee1553f9ad50e0a44adb4f01
Zain Ali <zain08816@gmail.com>
2020-03-24 19:48:27 -0500

    add some better api routes

--- backend/main/routes.js
+++ backend/main/routes.js
@@ -1,8 +1,38 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/api/hello', (req, res) => {
-    res.json('hello world')
+
+// Profile stuff
+
+router.post('/api/posts/signup', (req, res, next) => {
+    const values = [req.body.username,
+    req.body.password]
+    pool.query(`INSERT INTO login(username, password)
+                VALUES($1, $2,)
+                ON CONFLICT DO NOTHING`, values,
+        (q_err, q_res) => {
+            res.json(q_res.rows)
+        })
+})
+
+router.get('/api/get/getuser', (req, res, next) => {
+    const user = req.query.username
+    console.log(username)
+    pool.query(`SELECT * FROM login
+                WHERE username=$1`, [username],
+        (q_err, q_res) => {
+            res.json(q_res.rows)
+        })
+})
+
+router.get('/api/get/checkuser', (req, res, next) => {
+    const values = [req.query.username, req.query.passowrd]
+    console.log(username)
+    pool.query(`SELECT * FROM login
+                WHERE username=$1`, values,
+        (q_err, q_res) => {
+            res.json(q_res.rows)
+        })
 })
 
 module.exports = router
