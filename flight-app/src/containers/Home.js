1cd97fe8dfbf1c325f7535de5582ada8d7cc3aee
Zain Ali <zain08816@gmail.com>
2020-04-12 13:52:16 -0600

    add endpoint test

--- flight-app/src/containers/Home.js
+++ flight-app/src/containers/Home.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./Home.css";
+import axios from 'axios';
+
 
 export default function Home() {
+
+    const [hello, setHello] = useState('Not Hello')
+
+    function handleHello() {
+        axios.get('api/hello', {params: {word: "flightinator"}})
+            .then(res => setHello(res.data));
+    }
+
+
+
     return (
         <div className="Home">
             <div className="lander">
                 <h1>Flightinator 9000</h1>
                 <p>Book a flight here</p>
+                <button onClick={handleHello}>
+                    {hello}
+                </button>
             </div>
         </div>
     );
