32e004c4f8ed33c8750935f2d5edd08b82b4e166
Zain Ali <zain08816@gmail.com>
2020-04-12 13:52:00 -0600

    add working test endpoint

--- backend/main/routes.js
+++ backend/main/routes.js
@@ -3,10 +3,9 @@ var router = express.Router()
 
 
 // Profile stuff
-
+// SQL SYNTAX isnt correct here
 router.post('/api/posts/signup', (req, res, next) => {
-    const values = [req.body.username,
-    req.body.password]
+    const values = [req.body.username, req.body.password]
     pool.query(`INSERT INTO login(username, password)
                 VALUES($1, $2,)
                 ON CONFLICT DO NOTHING`, values,
@@ -19,7 +18,7 @@ router.get('/api/get/getuser', (req, res, next) => {
     const user = req.query.username
     console.log(username)
     pool.query(`SELECT * FROM login
-                WHERE username=$1`, [username],
+                WHERE username=arjuuun`, [username],
         (q_err, q_res) => {
             res.json(q_res.rows)
         })
@@ -29,10 +28,15 @@ router.get('/api/get/checkuser', (req, res, next) => {
     const values = [req.query.username, req.query.passowrd]
     console.log(username)
     pool.query(`SELECT * FROM login
-                WHERE username=$1`, values,
+                WHERE username=arjuuun`, values,
         (q_err, q_res) => {
             res.json(q_res.rows)
         })
+})
+
+router.get('/api/hello', (req, res, next) => {
+    const word = req.query.word
+	res.json('hello '+ word)
 })
 
 module.exports = router
