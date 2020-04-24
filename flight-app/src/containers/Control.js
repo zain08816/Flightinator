11dda974424093783867e4d9390e301f36dc9063
Zain Ali <zain08816@gmail.com>
2020-04-23 23:08:16 -0600

    fix search function

--- flight-app/src/containers/Control.js
+++ flight-app/src/containers/Control.js
@@ -1,37 +1,64 @@
 import React, { useState } from "react";
 import "./Control.css";
 import {
-    HelpBlock,
     FormGroup,
     FormControl,
     ControlLabel,
     Form,
-    Button,
     Radio
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
+import Cookies from 'universal-cookie'
+import axios from "axios";
 
 export default function Search(props) {
+
+    const cookies = new Cookies();
 
 
     const [fields, handleFieldChange] = useFormFields({
         departure: "",
         arrival: "",
-        trip_type: ""
+        trip_type: "all",
+        username: cookies.get('username')
     });
     const [recieved, setRecieved] = useState(false);
+    const [response, setResponse] = useState("");
 
     async function handleSubmit(event) {
+
+        axios.get('/api/posts/search_flight', {
+            departure: fields.departure,
+            arrival: fields.arrival,
+            trip_type: fields.trip_type
+        })
+        .then( res => {
+            const error = res.data.error;
+            if ( error ) {
+                setResponse(error.sqlMessage);
+                
+            } else {
+                setResponse(res.data.result)
+                setRecieved(true);
+            } 
+        }).catch( err => {
+            if(err) console.log(`API Error: ${err}`);
+        });
+
         event.preventDefault()
-    
         setRecieved(true);
+    
+        
     }
 
     function renderResults() {
         return (
             <div>
-                Render Search here
+                Search Results for {fields.username}, Departure: {fields.departure}, Arrival: {fields.arrival}, Trip type: {fields.trip_type}
+                <div>
+                    {response}
+                </div>
             </div>
         );
     }
@@ -43,10 +70,10 @@ export default function Search(props) {
             <div className="fs-container">
                 <Form onSubmit={handleSubmit} className="Control">
                 <h2 className="title">
-                    Search Flights Here {props.user}
+                    Hello {props.user}! Search flights here.
                 </h2>
                 <Form inline>
-                    <FormGroup controlId="formControlsSelect">
+                    <FormGroup controlId="departure">
                         {/* <ControlLabel>Departure</ControlLabel> */}
                         <FormControl
                             autoFocus
@@ -56,7 +83,7 @@ export default function Search(props) {
                             onChange={handleFieldChange}>
                         </FormControl>
                     </FormGroup>
-                    <FormGroup controlId="formControlsSelect">
+                    <FormGroup controlId="arrival">
                         {/* <ControlLabel>Arrival</ControlLabel> */}
                         <FormControl
                             type="text"
@@ -65,16 +92,18 @@ export default function Search(props) {
                             onChange={handleFieldChange}>
                         </FormControl>
                     </FormGroup>
-                    <FormGroup controlId="formControlsSelect">
-                        {/* <ControlLabel>Trip Type</ControlLabel> */}
+                    <FormGroup controlId="trip_type">
+
                         <FormControl
                             componentClass="select"
                             placeholder="select"
-                            value={fields.arrival}
+                            value={fields.trip_type}
                             onChange={handleFieldChange}>
-                            <option value="one">One Way</option>
-                            <option value="round">Round Trip</option>
-                            <option value="both">All</option>
+                            
+                            <option value="all" >Trip Type...</option>
+                            <option value="one" >One Way</option>
+                            <option value="round" >Round Trip</option>
+                            <option value="all" >All</option>
                         </FormControl>
                     </FormGroup>
                         
