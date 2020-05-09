b7f854bedfd29be6180e8258dfe1a9a3b6d47fce
Zain Ali <zain08816@gmail.com>
2020-05-09 14:03:31 -0600

    fix spelling

--- flight-app/src/containers/covid.js
+++ flight-app/src/containers/covid.js
@@ -9,15 +9,16 @@ export default function covid(){
         <div>
             <br/>
             <br/>
+            <h2>COVID-9 Information</h2>
             <ol>
-                <li>To reduce airport crowding only non-stop flights will be availible.</li>
+                <li>To reduce airport crowding only non-stop flights will be available.</li>
                 <li>Airlines have opted for a flat rate price due to flight status volatility. 
                     Price is fixed at the most premium rate i.e. booking a flight earlier will not reduce cost and vice versa</li>
                 <li>No flights will be delayed. All planes are filled with important cargo and cannot afford to be delayed. 
                     If a plane is delayed, another plane will replace it on time at no cost to the customer.</li>
                 <li>Airlines and Airports are requiring the use of face masks when in the terminal</li>
                 <li>All aircraft interiors are sanitized after every flight for passenger safety. 
-                    If you choose to bring your own sanitization wipes, please dispose of them when prompted by flight attendants</li>
+                    If you choose to bring your own sanitization wipes, please dispose of them when prompted by flight attendants.</li>
                 <li>In flight food and drink service has been suspended until further notice. This is done safetly of flight attendants.</li>
             </ol>
 
