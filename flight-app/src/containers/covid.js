b5148c3dd871fc637921d1d998b6edf8aaca3d73
Zain Ali <zain08816@gmail.com>
2020-05-09 06:27:01 -0600

    add covid notice

---
+++ flight-app/src/containers/covid.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+
+export default function covid(){
+
+
+    return(
+        <div>
+            <br/>
+            <br/>
+            <ol>
+                <li>To reduce airport crowding only non-stop flights will be availible.</li>
+                <li>Airlines have opted for a flat rate price due to flight status volatility. 
+                    Price is fixed at the most premium rate i.e. booking a flight earlier will not reduce cost and vice versa</li>
+                <li>No flights will be delayed. All planes are filled with important cargo and cannot afford to be delayed. 
+                    If a plane is delayed, another plane will replace it on time at no cost to the customer.</li>
+                <li>Airlines and Airports are requiring the use of face masks when in the terminal</li>
+                <li>All aircraft interiors are sanitized after every flight for passenger safety. 
+                    If you choose to bring your own sanitization wipes, please dispose of them when prompted by flight attendants</li>
+                <li>In flight food and drink service has been suspended until further notice. This is done safetly of flight attendants.</li>
+            </ol>
+
+        </div>
+
+
+
+    );
+}
